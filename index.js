71355182f5295b55cf2bab74edd7dbf00c40ca92
zanfack25 <zanfackdave@gmail.com>
2025-10-27 02:17:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -35,6 +35,6 @@ const server = http.createServer(function(request, response) {
 })
 
 const port = 3000
-const host = '127.0.0.1'
+const host = '0.0.0.0'
 server.listen(port, host)
 console.log(`Listening at http://${host}:${port}`)
